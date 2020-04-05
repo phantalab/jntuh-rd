df606b4ff95018134a46a9a9d6c02ad5e3a03bee
RIGVEDA <rigvedabarbie@gmail.com>
2020-04-05 23:53:45 +0530

    Firebase auth

--- rnd/views.py
+++ rnd/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+import pyrebase
+from django.shortcuts import render 
+
+
+
+config = {
+    'apiKey': "AIzaSyAwTQ43TVqlmBvMyXZjRI5UJAj70GZJ1CE",
+    'authDomain': "project-randd.firebaseapp.com",
+    'databaseURL': "https://project-randd.firebaseio.com",
+    'projectId' : "project-randd",
+    'storageBucket': "project-randd.appspot.com",
+    'messagingSenderId' : "700682061122",
+    'appId' : "1:700682061122:web:f258f9ec0dc888bafb34db",
+    'measurementId' : "G-1E8M7F2DC7"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+email = input("enter Email ID :")
+pwd = input("enter password : ")
+user = auth.sign_in_with_email_and_password(email,pwd)
+print("current user is ",user['localId'])
+
 
 # Create your views here.
 
