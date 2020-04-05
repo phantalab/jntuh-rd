4d8b9a219735413d97a53dfadf2668940ea520ff
phantalab <murtuza.labib@gmail.com>
2020-04-05 16:18:00 +0530

    inital

--- rnd/views.py
+++ rnd/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,'rnd/homep.html')
+    return render(request, 'rnd/homep.html')
+
 
 def login(request):
-    return render(request,'rnd/loginp.html')
+    return render(request, 'rnd/loginp.html')
+
 
 def prephd(request):
-    return render(request,'rnd/prephd.html')
+    return render(request, 'rnd/prephd.html')
+
 
 def resmtd(request):
-    return render(request,'rnd/resmtd.html')
+    return render(request, 'rnd/resmtd.html')
+
 
 def rrm(request):
-    return render(request,'rnd/rrm.html')
+    return render(request, 'rnd/rrm.html')
